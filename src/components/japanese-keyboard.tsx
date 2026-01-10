d8f59745c54bd79ea200ae603a14ff985caf8190
Rahmad aryadi <rahmadaryadi17@gmail.com>
2026-01-10 16:41:41 +0000

    tambahkan ~ di keyboard

--- src/components/japanese-keyboard.tsx
+++ src/components/japanese-keyboard.tsx
@@ -12,7 +12,7 @@ interface JapaneseKeyboardProps {
 const qwertyLayout = [
   ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
   ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-  ['z', 'x', 'c', 'v', 'b', 'n', 'm', '-'],
+  ['z', 'x', 'c', 'v', 'b', 'n', 'm', '-', '~'],
 ];
 
 export function JapaneseKeyboard({
